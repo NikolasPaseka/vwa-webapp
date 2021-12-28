77aa7444e664a3cf043472215a1c88b5d08074bc
Nikolas Paseka <nikolas.paseka7@gmail.com>
2021-12-28 22:03:03 +0100

    bug fixes, new functions
    password change page, fixed listing of empty habitats, fixed assigning majordoms, fixed moving out colonists, page for creating new droid, search bars in lists, css updates

--- src/routes.php
+++ src/routes.php
@@ -86,9 +86,9 @@ $app->get('/colonist/{id}/profile/edit', function (Request $request, Response $r
     // data 2D array, habitat 1D array
     $data['colonist'] = $stmt->fetch();
 
-    $stmt = $this->db->prepare('SELECT colonist.id_habitat, name, size, count(*) AS number_of_colonists FROM colonist 
-                                LEFT JOIN habitat ON colonist.id_habitat = habitat.id_habitat 
-                                GROUP BY colonist.id_habitat');
+    $stmt = $this->db->prepare('SELECT * FROM habitat 
+                                LEFT JOIN (SELECT count(*) AS number_of_colonists, id_habitat AS idh FROM colonist GROUP BY id_habitat) c 
+                                    ON habitat.id_habitat = c.idh');
     $stmt->execute();
     $data['habitats'] = $stmt->fetchall();
 
@@ -102,16 +102,29 @@ $app->get('/colonist/{id}/profile/edit', function (Request $request, Response $r
 
 $app->post('/colonist/{id}/profile/edit', function (Request $request, Response $response, $args) {
     $formData = $request->getParsedBody();
+
+    $stmt = $this->db->prepare('SELECT id_habitat, authorization FROM colonist
+                                WHERE id_colonist = :id');
+    $stmt->bindValue(':id', $args['id']);
+    $stmt->execute();
+    $colonist = $stmt->fetch();
+    // check if colonist is majordom and is being moved out
+    $auth = $colonist['authorization'];
+    if ($colonist['id_habitat'] != $formData['habitat']) {
+        $auth = 'colonist';
+    }
+
     if (checkHabitatCapacity($this, $formData['habitat'])) {
         try {
             $stmt = $this->db->prepare('UPDATE colonist SET firstname = :fn, lastname = :ln, gender = :ge,
-                                        username = :un, id_habitat = :idh
+                                        username = :un, id_habitat = :idh, authorization = :auth
                                         WHERE id_colonist = :idc');
             $stmt->bindValue(':fn', $formData['firstname']);
             $stmt->bindValue(':ln', $formData['lastname']);
             $stmt->bindValue(':ge', $formData['gender']);
             $stmt->bindValue(':un', $formData['username']);
             $stmt->bindValue(':idh', $formData['habitat']);
+            $stmt->bindValue(':auth', $auth);
             $stmt->bindValue(':idc', $args['id']);
             $stmt->execute();
 
@@ -131,6 +144,46 @@ $app->post('/colonist/{id}/profile/edit', function (Request $request, Response $
     $_SESSION['message'] = $data['message'];
 
     return $response->withRedirect($this->router->pathFor('colonist_profile_edit', $args));
+});
+
+// colonist password change
+$app->get('/colonist/change_password', function (Request $request, Response $response, $args) {
+    return $this->view->render($response, 'password_change.latte');
+})->setName('colonist_change_password');
+
+$app->post('/colonist/change_password', function (Request $request, Response $response, $args) {
+    $formData = $request->getParsedBody();
+
+    // check old password
+    $password_hash['pass'] = hash('sha256', $formData['old_password']);
+    $stmt = $this->db->prepare('SELECT password FROM colonist
+                                WHERE id_colonist = :id');
+    $stmt->bindValue(':id', $_SESSION['logged_colonist']['id_colonist']);
+    $stmt->execute();
+    $password_database = $stmt->fetch();
+
+    if ($password_hash['pass'] == $password_database['password']) {
+        if ($formData['new_password1'] == $formData['new_password2']) {
+            $newPasswordHash = hash('sha256', $formData['new_password1']);
+            try {
+                $stmt = $this->db->prepare('UPDATE colonist SET password = :pass
+                                            WHERE id_colonist = :id');
+                $stmt->bindValue(':pass', $newPasswordHash);
+                $stmt->bindValue(':id', $_SESSION['logged_colonist']['id_colonist']);
+                $stmt->execute();
+
+                $data['message'] = "Password succesfuly updated";
+            } catch (Exception $e) {
+                $data['message'] = $e;
+            }
+        } else {
+            $data['message'] = "Passwords dont match up";
+        }
+    } else {
+        $data['message'] = "Wrong old password";
+    }
+
+    return $this->view->render($response, 'password_change.latte', $data);
 });
 
 // create new colonist
@@ -194,50 +247,88 @@ $app->get('/colonist/profile/sell_droid', function (Request $request, Response $
 
 // list of all colonists
 $app->get('/colonist/list/all', function (Request $request, Response $response, $args) {
-    $stmt = $this->db->prepare('SELECT * FROM colonist');
-    $stmt->execute();
-    $data['colonists'] = $stmt->fetchall();
+    $params = $request->getQueryParams();
 
-    $data['filter'] = "All colonists";
-
+    if (empty($params['search'])) {
+        $stmt = $this->db->prepare('SELECT * FROM colonist');
+        $stmt->execute();
+        $data['colonists'] = $stmt->fetchall();
+        
+        $data['filter'] = "All colonists";
+    } else {
+        $stmt = $this->db->prepare('SELECT * FROM colonist 
+                                     WHERE lower(firstname) = lower(:search) OR lower(lastname) = lower(:search) OR lower(username) = lower(:search)');
+        $stmt->bindValue(':search', $params['search']);
+        $stmt->execute();
+        $data['colonists'] = $stmt->fetchall();
+        
+        $data['filter'] = "Search query: ".$params['search'];
+    }
     return $this->view->render($response, 'colonist_list.latte', $data);
 })->setName('colonist_list_all');
 
 // list of colonists set filter
 $app->get('/colonist/list/habitat', function (Request $request, Response $response, $args) {
-    $stmt = $this->db->prepare('SELECT * FROM colonist WHERE id_habitat = :loggidhab');
-    $logged_colonist = $_SESSION['logged_colonist'];
-    $stmt->bindValue(':loggidhab', $logged_colonist['id_habitat']);
-    $stmt->execute();
-    $data['colonists'] = $stmt->fetchall();
+    $params = $request->getQueryParams();
 
-    $data['filter'] = "Colonists in same habitat";
+    if (empty($params['search'])) {
+        $stmt = $this->db->prepare('SELECT * FROM colonist WHERE id_habitat = :loggidhab');
+        $logged_colonist = $_SESSION['logged_colonist'];
+        $stmt->bindValue(':loggidhab', $logged_colonist['id_habitat']);
+        $stmt->execute();
+        $data['colonists'] = $stmt->fetchall();
+
+        $data['filter'] = "Colonists in same habitat";
+    } else {
+        $stmt = $this->db->prepare('SELECT * FROM colonist 
+                                    WHERE lower(firstname) = lower(:search) OR lower(lastname) = lower(:search) OR lower(username) = lower(:search)');
+        $stmt->bindValue(':search', $params['search']);
+        $stmt->execute();
+        $data['colonists'] = $stmt->fetchall();
+        
+        $data['filter'] = "Search query: ".$params['search'];
+    }
 
     return $this->view->render($response, 'colonist_list.latte', $data);
 })->setName('colonist_list_habitat');
 
 // list habitats
 $app->get('/habitat/list', function (Request $request, Response $response, $args) {
-    $stmt = $this->db->prepare('SELECT *, count(*) AS actual_capacity FROM colonist 
-                                LEFT JOIN habitat ON habitat.id_habitat = colonist.id_habitat
-                                LEFT JOIN (SELECT username AS majordom, id_habitat AS id_h FROM colonist WHERE authorization = :auth) c 
-                                    ON habitat.id_habitat = c.id_h
-                                GROUP BY colonist.id_habitat');
-    $stmt->bindValue(':auth', "majordom");
-    $stmt->execute();
-    $data['habitats'] = $stmt->fetchall();
+    $params = $request->getQueryParams();
+
+    if (empty($params['search'])) {
+        $stmt = $this->db->prepare('SELECT * FROM habitat 
+                                    LEFT JOIN (SELECT count(*) AS actual_capacity, id_habitat AS idh FROM colonist 
+                                                GROUP BY id_habitat) c ON habitat.id_habitat = c.idh
+                                    LEFT JOIN (SELECT username AS majordom, id_habitat AS idh FROM colonist WHERE authorization = :auth) a 
+                                    ON habitat.id_habitat = a.idh');
+        $stmt->bindValue(':auth', "majordom");
+        $stmt->execute();
+        $data['habitats'] = $stmt->fetchall();
+    } else {
+        $stmt = $this->db->prepare('SELECT * FROM habitat 
+                                    LEFT JOIN (SELECT count(*) AS actual_capacity, id_habitat AS idh FROM colonist 
+                                                GROUP BY id_habitat) c ON habitat.id_habitat = c.idh
+                                    LEFT JOIN (SELECT username AS majordom, id_habitat AS idh FROM colonist WHERE authorization = :auth) a 
+                                    ON habitat.id_habitat = a.idh
+                                    WHERE lower(name) = lower(:search)');
+        $stmt->bindValue(':auth', "majordom");
+        $stmt->bindValue(':search', $params['search']);
+        $stmt->execute();
+        $data['habitats'] = $stmt->fetchall();
+    }
 
     return $this->view->render($response, 'habitat_list.latte', $data);
 })->setName('habitats_list');
 
 // habitat info page
 $app->get('/habitat/{id}/info', function (Request $request, Response $response, $args) {
-    $stmt = $this->db->prepare('SELECT *, count(*) AS actual_capacity FROM colonist 
-                                LEFT JOIN habitat ON habitat.id_habitat = colonist.id_habitat
-                                LEFT JOIN (SELECT username AS majordom, id_habitat AS id_h FROM colonist WHERE authorization = :auth) c 
-                                    ON habitat.id_habitat = c.id_h
-                                WHERE habitat.id_habitat = :idh
-                                GROUP BY colonist.id_habitat');
+    $stmt = $this->db->prepare('SELECT * FROM habitat 
+                                LEFT JOIN (SELECT count(*) AS actual_capacity, id_habitat AS idh FROM colonist 
+                                            GROUP BY id_habitat) c ON habitat.id_habitat = c.idh
+                                LEFT JOIN (SELECT username AS majordom, id_habitat AS idh FROM colonist WHERE authorization = :auth) a 
+                                ON habitat.id_habitat = a.idh
+                                WHERE id_habitat = :idh');
     $stmt->bindValue(':auth', "majordom");
     $stmt->bindValue(":idh", $args['id']);
     $stmt->execute();
@@ -266,9 +357,15 @@ $app->get('/habitat/{id}/info/edit', function (Request $request, Response $respo
     $stmt = $this->db->prepare('SELECT * FROM habitat WHERE id_habitat = :idh');
     $stmt->bindValue(':idh', $args['id']);
     $stmt->execute();
-
-    // data 2D array, habitat 1D array
     $data['habitat'] = $stmt->fetch();
+
+    $stmt = $this->db->prepare('SELECT * FROM colonist 
+                                JOIN (SELECT name, id_habitat FROM habitat) h on h.id_habitat = colonist.id_habitat
+                                WHERE authorization = :auth1 OR authorization = :auth2');
+    $stmt->bindValue(':auth1', 'colonist');
+    $stmt->bindValue(':auth2', 'majordom');
+    $stmt->execute();
+    $data['colonists'] = $stmt->fetchAll();
 
     if ($_SESSION['message']) {
         $data['message'] = $_SESSION['message'];
@@ -280,6 +377,16 @@ $app->get('/habitat/{id}/info/edit', function (Request $request, Response $respo
 
 $app->post('/habitat/{id}/info/edit', function (Request $request, Response $response, $args) {
     $formData = $request->getParsedBody();
+
+    // update majordom
+    $stmt = $this->db->prepare('UPDATE colonist SET id_habitat = :idh, authorization = :auth
+                                WHERE id_colonist = :idc');
+    $stmt->bindValue(':idh', $args['id']);
+    $stmt->bindValue(':auth', 'majordom');
+    $stmt->bindValue(':idc', $formData['colonist']);
+    $stmt->execute();
+
+
     if (getHabitatCapacity($this, $args['id']) <= $formData['size']) {
         try {
             $stmt = $this->db->prepare('UPDATE habitat SET name = :name, coordinate_x = :coorx, coordinate_y = :coory,
@@ -414,3 +521,28 @@ $app->get('/droid/shop/{model}/{id}/buy', function (Request $request, Response $
 
     return $response->withRedirect($this->router->pathFor('droid_shop', $args));
 })->setName('droid_shop_buy');
+
+// create new droid
+$app->get('/droid/create', function (Request $request, Response $response, $args) {
+    return $this->view->render($response, 'droid_new.latte');
+})->setName('droid_new');
+
+$app->post('/droid/create', function (Request $request, Response $response, $args) {
+    $formData = $request->getParsedBody();
+    try {
+        $stmt = $this->db->prepare('INSERT INTO droid (model, color, class, condition, price)
+                                    VALUES (:model, :color, :class, :con, :price)');
+        $stmt->bindValue(':model', $formData['model']);
+        $stmt->bindValue(':color', $formData['color']);
+        $stmt->bindValue(':class', $formData['class']);
+        $stmt->bindValue(':con', $formData['condition']);
+        $stmt->bindValue(':price', $formData['price']);
+
+        $stmt->execute();
+
+        $data['message'] = 'New droid sucessfuly created';
+    } catch (Exception $e) {
+        $data['message'] = $e;
+    }
+    return $this->view->render($response, 'droid_new.latte', $data);
+});
